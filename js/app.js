4eb43c278f9e7033b4e6ec3a4532ad06434198e3
EmanAlazzam <emanalazzam577@gmail.com>
2021-03-12 22:16:00 +0200

    lab 6 first branch

--- js/app.js
+++ js/app.js
@@ -1,93 +1,129 @@
 'use strict';
 
+//Hour Array To Specify the the time of opening and closing.
 let hour=['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm'];
+
+
+//Creating an unordered list for each object.
+function render (branch)
+{
+  //Creating The Unorder List For Each Object.
+  let Elemens=document.getElementById('list');
+  let unOrderList=document.createElement('ul');
+  Elemens.appendChild(unOrderList);
+
+  //Creating An Element For The City Name.
+  let cityName=document.createElement('p');
+  unOrderList.appendChild(cityName);
+  cityName.textContent=branch.name;
+
+  //Creating The Lists For Each Random Number.
+  for(let j=0;j<hour.length;j++)
+  {
+    let numOfTotal=branch.avgOfCockiesForEachPerson();
+    let lists=document.createElement('li');
+    unOrderList.appendChild(lists);
+    branch.total+=numOfTotal;
+    lists.textContent=hour[j]+': '+numOfTotal+' of Cookies.';
+  }
+
+  console.log(branch.total);
+  //Unodered list for the summation of cookies ber day.
+  let lists=document.createElement('li');
+  unOrderList.appendChild(lists);
+  lists.textContent='Total'+': '+branch.total+' of Cookies.';
+
+}
+
+//Declearing The Objects For The 5 Different Locations.
 let Seattle={
   max:65,
   min:23,
-  avg:6.3,
-  hour:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm'],
-  name:'seattle',
   total:0,
-  avgOfCockies:[],
-
+  avg:6.3,
+  name:'Seattle',
   randomCus: function(){
-    for (let i=0;i<this.hour.length ;i++){
-      let randCustomer= (Math.floor(Math.random() * (this.max - this.min+1) + this.min));
-      return randCustomer;
-    }
+    let randCustomer= (Math.floor(Math.random() * (this.max - this.min+1) + this.min));
+    return randCustomer;
   },
   avgOfCockiesForEachPerson: function() {
-    for(let j=0;j<this.hour.length;j++){
-      this.avgOfCockies=(Math.floor(this.avg * this.randomCus()));
-      this.total=this.total+this.avgOfCockies[j]; //this keep give me a zero as the sum 
-    }
+    let avgOfCockies=(Math.floor(this.avg * this.randomCus()));
+    return avgOfCockies;
   },
-  dom: function() {
-    document.getElementById('test').innerText=this.name;
-    let prg=document.getElementById('lists');
-    let unOrderedList=document.createElement('ul');
-    for(let c=0;c<this.hour.length;c++){
-      let lists=document.createElement('li');
-      lists.innerText=[hour[c]+': '+this.hourArry[c]];
-      unOrderedList.appendChild(lists);
-      prg.appendChild(unOrderedList);
-    }
-    document.getElementById('test2').innerText=this.total;
-  }
 };
 
-console.log(Seattle.randomCus());
-console.log(Seattle.avgOfCockiesForEachPerson());
-console.log(Seattle.dom());
+//
+let Tokyo={
+  max: 24,
+  min: 3,
+  avg: 1.2,
+  name:'Tokyo',
+  total:0,
+  randomCus: function(){
+    let randCustomer= (Math.floor(Math.random() * (this.max - this.min+1) + this.min));
+    return randCustomer;
+  },
+  avgOfCockiesForEachPerson: function() {
+    let avgOfCockies=(Math.floor(this.avg * this.randomCus()));
+    return avgOfCockies;
+  },};
+
+//
+let Dubai={
+  max: 38,
+  min: 11,
+  avg: 3.7,
+  name:'Dubai',
+  total:0,
+  randomCus: function(){
+    let randCustomer= (Math.floor(Math.random() * (this.max - this.min+1) + this.min));
+    return randCustomer;
+  },
+  avgOfCockiesForEachPerson: function() {
+    let avgOfCockies=(Math.floor(this.avg * this.randomCus()));
+    return avgOfCockies;
+  },
+};
+
+//
+let Paris={
+  max: 38,
+  min: 20,
+  avg: 2.3,
+  name:'Paris',
+  total:0,
+  randomCus: function(){
+    let randCustomer= (Math.floor(Math.random() * (this.max - this.min+1) + this.min));
+    return randCustomer;
+  },
+  avgOfCockiesForEachPerson: function() {
+    let avgOfCockies=(Math.floor(this.avg * this.randomCus()));
+    return avgOfCockies;
+  },
+};
+
+//
+let Lima={
+  max: 16,
+  min: 2,
+  avg: 4.6,
+  name:'Lima',
+  total:0,
+  randomCus: function(){
+    let randCustomer= (Math.floor(Math.random() * (this.max - this.min+1) + this.min));
+    return randCustomer;
+  },
+  avgOfCockiesForEachPerson: function() {
+    let avgOfCockies=(Math.floor(this.avg * this.randomCus()));
+    return avgOfCockies;
+  },};
 
 
 
 
-// function hour(hourArry) {
-//   for(let i=0;i<=16;i++){
-//     avgArry[i]=[hourArry[i]+seattle.avgOfCockies];
-//     console.log(avgArry);
-//   }
-// let seattle={
-//   name: 'jordan',
-//   max:65,
-//   min: 23,
-//   avg: 6.3,
-//   avgOfCockies: function(){
-//     let randCustomer= Math.floor(Math.random() * (this.max - this.min) + this.min);
-//     return randCustomer;
-//   }
-// };
-// console.log(seattle.randCustomer());
-
-//   for(let i=6;i<12;i++){
-//     hourArry[i]=[i+'am: '+this.avgOfCockies()];
-//   }
-//   for(let i=12;i<20;i++){
-//     hourArry[i]=[i+'pm: '+this.avgOfCockies()];
-//   }
-//   return hourArry;
-// }
-
-
-//   Tokyo:{
-//     max: 24,
-//     min: 3,
-//     avg: 1.2,
-//   },
-//   Dubai:{
-//     max: 38,
-//     min: 11,
-//     avg: 3.7,
-//   },
-//   Paris:{
-//     max: 38,
-//     min: 20,
-//     avg: 2.3,
-//   },
-//   Lima:{
-//     max: 16,
-//     min: 2,
-//     avg: 4.6,
-//   },
-// };
+// Calling The Objects By "Name of the object" Inside A Render Function.
+render(Seattle);
+render(Tokyo);
+render(Dubai);
+render(Paris);
+render(Lima);
